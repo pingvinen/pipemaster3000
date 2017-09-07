86e688af7dc61e2030f2922aa9278a305dd7d521
Patrick Boisen <patrick@skarp.dk>
2017-09-07 14:27:40 +0200

    Copy graph rendering stuff to PipelineScene

--- ui/src/routes/pipeline/PipelineScene.jsx
+++ ui/src/routes/pipeline/PipelineScene.jsx
@@ -1,6 +1,8 @@
 import {h, Component} from 'preact';
 import style from './pipeline.css';
 import api from 'api';
+import Graph from 'react-graph-vis';
+import SelectedNodeInfo from './components/SelectedNodeInfo';
 
 export default class PipelineScene extends Component {
 
@@ -9,9 +11,17 @@ export default class PipelineScene extends Component {
 
         this.componentDidMount = this.componentDidMount.bind(this);
         this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
+        this.onClickNode = this.onClickNode.bind(this);
+        this.getGraph = this.getGraph.bind(this);
 
         this.state = {
-            traces: []
+            traces: [],
+            lookup: {},
+            selected: {
+                component: '',
+                received: '',
+                sent: ''
+            }
         };
     }
 
@@ -19,7 +29,12 @@ export default class PipelineScene extends Component {
         const response = await api.get(`/api/traces/${correlationId}`);
 
         this.setState({
-            traces: response.items
+            traces: response.items,
+            selected: {
+                component: '',
+                received: '',
+                sent: ''
+            }
         });
     }
 
@@ -28,19 +43,134 @@ export default class PipelineScene extends Component {
     }
 
     async componentWillReceiveProps(nextProps) {
-        await this.getTraces(nextProps.correlationId);
+        if (nextProps.correlationId !== this.props.correlationId) {
+            await this.getTraces(nextProps.correlationId);
+
+        }
+    }
+
+    onClickNode(e) {
+        if (!e.nodes || e.nodes.length === 0) {
+            // user clicked something other than a node
+            this.setState({
+                selected: {
+                    component: '',
+                    received: '',
+                    sent: ''
+                }
+            });
+            return;
+        }
+
+        const nodeInfo = this.state.lookup[e.nodes[0]];
+
+        this.setState({
+            selected: {
+                component: nodeInfo.component,
+                received: nodeInfo.received,
+                sent: nodeInfo.sent
+            }
+        });
+    }
+
+    setLookup(node, key, value) {
+        const state = this.state;
+
+        if (!state.lookup[node]) {
+            state.lookup[node] = {};
+        }
+
+        state.lookup[node][key] = value;
+    }
+
+    getNodeId(component) {
+        return `n${component}`;
+    }
+
+    getGraph(traces) {
+        const nodes = {};
+        const edges = {};
+
+        // make nodes
+        traces.forEach(t => {
+            const nodeId = this.getNodeId(t.sender);
+
+            if (!nodes[t.sender]) {
+                nodes[t.sender] = {
+                    id: nodeId,
+                    label: t.sender
+                };
+            }
+
+            // make lookup
+            this.setLookup(nodeId, 'component', t.sender);
+            this.setLookup(nodeId, t.action, t.payload);
+        });
+
+        // make edges
+        traces.forEach(t => {
+            const path = t.path;
+
+            if (path.length > 1) {
+                const fromNode = path[path.length-2];
+                const toNode = path[path.length-1];
+
+                const id = `${fromNode}_to_${toNode}`;
+
+                if (!edges[id]) {
+                    edges[id] = {
+                        from: this.getNodeId(fromNode),
+                        to: this.getNodeId(toNode)
+                    };
+                }
+            }
+        });
+
+        return {
+            nodes: Object.values(nodes),
+            edges: Object.values(edges)
+        };
     }
 
     render(props, state) {
+        let graph = this.getGraph(state.traces);
+
+        const options = {
+            layout: {
+                hierarchical: {
+                    enabled: true,
+                    direction: "LR",
+                    parentCentralization: false,
+                    sortMethod: "directed"
+                }
+            },
+            edges: {
+                color: "#000000"
+            },
+            nodes: {
+                physics: false
+            }
+        };
+
+        const events = {
+            select: this.onClickNode
+        };
+
         return (
             <div class={style.this}>
                 <h1>Pipeline {props.correlationId}</h1>
 
-                <pre>
-                {state.traces.map(x => {
-                    return `${JSON.stringify(x)}\n`;
-                })}
-                </pre>
+                <Graph
+                    graph={graph}
+                    options={options}
+                    events={events}
+                />
+
+                <SelectedNodeInfo
+                    component={state.selected.component}
+                    received={state.selected.received}
+                    sent={state.selected.sent}
+                />
             </div>
         );
     }
