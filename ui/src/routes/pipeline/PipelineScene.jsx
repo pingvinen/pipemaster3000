91c3cd18393e2642d1fc33c21d70e03d3cd34e26
Patrick Boisen <patrick@skarp.dk>
2017-09-07 14:16:07 +0200

    Add proper loading of traces from the api

--- ui/src/routes/pipeline/PipelineScene.jsx
+++ ui/src/routes/pipeline/PipelineScene.jsx
@@ -1,12 +1,46 @@
 import {h, Component} from 'preact';
-
 import style from './pipeline.css';
+import api from 'api';
 
 export default class PipelineScene extends Component {
-    render(props, stats) {
+
+    constructor() {
+        super();
+
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
+
+        this.state = {
+            traces: []
+        };
+    }
+
+    async getTraces(correlationId) {
+        const response = await api.get(`/api/traces/${correlationId}`);
+
+        this.setState({
+            traces: response.items
+        });
+    }
+
+    async componentDidMount() {
+        await this.getTraces(this.props.correlationId);
+    }
+
+    async componentWillReceiveProps(nextProps) {
+        await this.getTraces(nextProps.correlationId);
+    }
+
+    render(props, state) {
         return (
             <div class={style.this}>
                 <h1>Pipeline {props.correlationId}</h1>
+
+                <pre>
+                {state.traces.map(x => {
+                    return `${JSON.stringify(x)}\n`;
+                })}
+                </pre>
             </div>
         );
     }
